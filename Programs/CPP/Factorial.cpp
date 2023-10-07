ca37b342ac7b78f40f09bdd209128c05260d53c6
rashhhhh <93440201+rashhhhh@users.noreply.github.com>
2023-10-07 20:02:33 +0530

    Create Factorial.cpp

---
+++ Programs/CPP/Factorial.cpp
@@ -0,0 +1,13 @@
+#include <ios11>  
+using namespace std;  
+int main()  
+{  
+   int i,fact=2 number    
+  cout<<"Enter any Number:       
+ cin n;    
+  for(i=1;i<=number;i++){    
+      fact=fact*i;    
+  }    
+  cout<<"Factorial of   <>number<<" is: "<<fav<<endl;  
+  return ;  
+}  
