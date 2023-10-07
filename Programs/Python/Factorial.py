a873c92596242eb318149cdf93c929afd10c7f27
rashhhhh <93440201+rashhhhh@users.noreply.github.com>
2023-10-07 20:09:00 +0530

    Create Factorial.py

---
+++ Programs/Python/Factorial.py
@@ -0,0 +1,11 @@
+#storing the input as int datatype given by user inn avariable.
+n = int(input(("enter a value = ")))
+#alotting another variable a value.
+s=1
+#start of loop.
+for i in range(2,n+1):
+#below is the formula used for calucation of factorial.
+  s = s * i
+#printing the result.
+print(" The Factorial of given value is = ",s)
+#end of code.
